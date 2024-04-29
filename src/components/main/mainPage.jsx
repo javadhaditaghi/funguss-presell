c39f388991fcffe911f70c171d9ca974695d4cb7
javad <javadht1990a@gmail.com>
2024-04-29 14:49:19 +0330

    Add the frequently asked question part

--- src/components/main/mainPage.jsx
+++ src/components/main/mainPage.jsx
@@ -4,6 +4,7 @@ import ArticleHeader from "./articleHeader/articleHeader";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
 import Product from "./product/product";
 import Footer from "./footer/footer";
+import FAQ from "./FAQ.jsx/faq";
 
 
 
@@ -18,18 +19,15 @@ const MainPage = () => {
       <ArticleHeader />
       <Grid2 container>
         <Grid2 xs={0} md={.5} lg={1.5}>
-
         </Grid2>
         <Grid2 xs={11} md={11} lg={9} sx={{ m: "auto" }}>
-
           <Product />
-
+          <FAQ />
         </Grid2>
         <Grid2 xs={0} md={.5} lg={1.5}>
         </Grid2>
-
-
       </Grid2>
+
       <Footer />
     </>
 
