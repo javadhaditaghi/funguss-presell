d0f84a7ffdd38220c7e8ed24326eff86a19c234b
javad <javadht1990a@gmail.com>
2024-05-06 11:46:04 +0330

    finish the left side of the contact us part

---
+++ src/components/consts/constants.jsx
@@ -0,0 +1,52 @@
+import { Website, Phone, Email, Address, Facebook, Twitter, Instagram, Youtube } from "../icons/contactUsIcons";
+
+const ContactInfo = [
+    {
+        icon: <Phone />,
+        title: "Phone",
+        value: "+123-234-1234"
+    },
+    {
+        icon: <Email />,
+        title: "Email",
+        value: "Funguss@gmail.com"
+    },
+    {
+        icon: <Website />,
+        title: "Website",
+        value: "www.funguss.com"
+    },
+    {
+        icon: <Address />,
+        title: "Address",
+        value: "22 Main Street"
+    }
+
+
+];
+
+const SocialNewtworks = [
+    {
+        icon: <Facebook />,
+    }
+    ,
+    {
+        icon: <Twitter />,
+    }
+    ,
+    {
+        icon: <Instagram />,
+    }
+    ,
+    {
+        icon: <Youtube />
+    }
+    ,
+
+
+
+
+]
+
+
+export { ContactInfo, SocialNewtworks };
