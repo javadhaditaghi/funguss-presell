41317dfb63ae756d859790433c00aa42331895a9
javad <javadht1990a@gmail.com>
2024-09-04 23:29:15 +0330

    add minor changes

--- src/components/main/header/topText.jsx
+++ src/components/main/header/topText.jsx
@@ -1,20 +1,26 @@
-import { Typography, Button } from "@mui/material"
+import { Typography, Button, Box } from "@mui/material"
 
 
 
 const TopText = () => {
     return (
         <>
-            <Typography sx={{ paddingTop: { xs: "160px", md: "150px", xl: "180px" } }} fontFamily={"Poppins"} color={"#5C645E"} fontWeight={600} letterSpacing={2.4}>
+            <Box sx={{ paddingTop: { xs: "140px", md: "150px", xl: "160px" } }}>
+                <Typography sx={{ display: { xs: "none", md: "block" } }} fontFamily={"Poppins"} color={"#5C645E"} fontWeight={600} letterSpacing={2.4}>
                 WELCOME TO CONSUMER MAG
-            </Typography>
-            <Typography fontFamily={"Poppins"} color={"#2D3928"} fontSize={{ xs: 30, md: 36, lg: 42, xl: 47 }} fontWeight={600}>
-                Your Ultimate Guide to Healthy Toenails
-            </Typography>
-            <Typography fontFamily={"DM Sans"} color={"#3A3A3A"} fontSize={"17.6px"} fontWeight={400} sx={{ display: { xs: "none", md: "block" } }}>
-                Consumer Magazine is the #1 Website to find out the best fungal infection treatments chosen by experts.
-            </Typography>
-            {/* <Button variant="contained" sx={{ marginTop: "15px", backgroundColor: "#5C645E" }} >MAKE APPOINMENT</Button> */}
+                </Typography>
+                <Typography fontFamily={"Poppins"} color={"#2D3928"} fontSize={{ xs: 30, md: 36, lg: 42, xl: 47 }} fontWeight={600}>
+                    Your Ultimate Guide to Healthy Toenails
+                </Typography>
+                <Typography fontFamily={"DM Sans"} color={"#3A3A3A"} fontSize={"17.6px"} fontWeight={400} sx={{ display: { xs: "none", md: "block" } }}>
+                    Consumer Magazine is the #1 Website to find out the best fungal infection treatments chosen by experts.
+                </Typography>
+
+                <Typography sx={{ display: { xs: "block", md: "none" } }} fontFamily={"Poppins"} color={"#5C645E"} fontWeight={600} letterSpacing={2.4}>
+                    Make sure you read this before purchase.
+                </Typography>
+                {/* <Button variant="contained" sx={{ marginTop: "15px", backgroundColor: "#5C645E" }} >MAKE APPOINMENT</Button> */}
+            </Box>
         </>
     )
 }
