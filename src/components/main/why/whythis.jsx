19eeffba94f27170db1847fdeccdc2f85106c2ff
javad <javadht1990a@gmail.com>
2024-10-07 13:40:15 +0330

    Add the why we chose this product dynamic text

--- src/components/main/why/whythis.jsx
+++ src/components/main/why/whythis.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import {Typography, Box} from '@mui/material';
-import jsonData from "./whythis.json"
+import jsonData from "./whythis.json";
+import perser from 'html-react-parser';
 
 
 
@@ -14,7 +14,14 @@ export default function WhyThis() {
         <>
             <Typography variant='h3' fontFamily={"Oswald"} pb={3}>Why Golden Nutrition Funguss</Typography>
             <Box sx={{backgroundColor: "#f1f1f1", padding: "15px", border: "1px solid rgba(0, 0, 0, 0.12)"}}>
-            {jsonData.text}
+                <Typography sx={{mb:1}}>{jsonData.introduction}</Typography>
+                <Typography sx={{mb:1}}>{perser(jsonData.text)}</Typography>
+                <Typography sx={{mb:1}}>{perser(jsonData.text1)}</Typography>
+                <Typography sx={{mb:1}}>{perser(jsonData.text2)}</Typography>
+                <Typography sx={{mb:1}}>{perser(jsonData.text3)}</Typography>
+                <Typography sx={{mb:1}}>{perser(jsonData.text4)}</Typography>
+                <Typography sx={{mb:1}}>{perser(jsonData.text5)}</Typography>
+                <Typography sx={{mb:1}}>{perser(jsonData.text6)}</Typography>
             </Box>
 
         </>
